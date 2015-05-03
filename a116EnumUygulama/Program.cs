030617e243f836b185825945b352a4f976c6df17
erdoganemrah <emraherdogan@outlook.com.tr>
2015-05-03 12:26:53 +0300

    Enum Uygulama

---
+++ a116EnumUygulama/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a116EnumUygulama
+{
+    class Program
+    {
+        /*
+         * Usertype isminde bir enum tip oluşturun. Bu tipe ait alınabilecek değerler Admin, Mod, User, Guest olabilsin.
+         * 
+         * Bu tipte bir değişken tanımlayarak istediğiniz bir değeri atamayı deneyin.
+         */
+        enum UserType
+        {
+            Admin = 1,
+            Mod = 2,
+            User = 3,
+            Guest = 0
+        }
+
+        static void Main(string[] args)
+        {
+            UserType user = UserType.Admin;
+
+            string userType = "";
+            user = (UserType)Enum.Parse(typeof(UserType), userType);
+
+            Console.WriteLine(user);
+            Console.ReadLine();
+        }
+    }
+}
